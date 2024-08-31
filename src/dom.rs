b8e1f78a750ce8465189a95a493183c7ee48720e
Ayush Krishna Murthi <kmayush27399@gmail.com>
2024-08-31 13:07:19 +0530

    declare DOM data structure

---
+++ src/dom.rs
@@ -0,0 +1,36 @@
+use std::collections::{HashMap, HashSet};
+
+// DOM tree
+struct Node {
+    children:Vec<Node>,
+    node_type: NodeType,
+}
+
+// Node can be either text or element.
+enum NodeType {
+    Text(String),
+    Element(ElementData),
+}
+
+// Element is consist of tagname and attrs, <title>Attribute
+struct ElementData {
+    tag_name: String,
+    attrs:AttrMap,
+}
+
+// Stores Attribute map like class="grid mt-2"
+type AttrMap = HashMap<String, String>;
+
+// Store to Node when element is text
+fn text(data:String) -> Node {
+    Node {children: Vec::new(), node_type: NodeType::Text(data)}
+}
+
+// Store to Node when element is element
+// Example input : tag = div, attrs = class:container-fluid, child nodes 
+pub fn element(tag_name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children, 
+        node_type: NodeType :: Element(ElementData{ tag_name, attrs })
+    }
+}
